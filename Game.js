c9a2f1a0e0d4ef6ca2373f345ec5cce34bc2dcc0
Thales Minoru Sato <thalesms2@outlook.com>
2022-03-24 11:15:04 -0300

    making the game winnable

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-import { size, X, O, BOARD, EMPTY} from './constants.js'
+import { size, X, O, BOARD, EMPTY, TURNS } from './constants.js'
 import { generateGameBoard } from './utils.js'
 
 class Game {
@@ -7,11 +7,11 @@ class Game {
         this.canvas.width = size.width
         this.canvas.height = size.height
         this.context = this.canvas.getContext('2d')
-        this.drawDesk(this.context)
+        this.drawDesk()
         this.board = generateGameBoard(BOARD)
     }
     
-    drawDesk(context) {
+    drawDesk() {
         this.context.beginPath()
         this.context.fillStyle = '#dcdcdc'
         this.context.strokeStyle = '#fff'
@@ -19,7 +19,8 @@ class Game {
         this.context.closePath()
         this.drawLines(this.context)
     }
-    drawLines(context) {
+
+    drawLines() {
         this.context.beginPath()
         this.context.lineWidth = 4
         this.context.moveTo(198, 0)
@@ -44,6 +45,7 @@ class Game {
         this.context.lineTo(50 + (x * 200), 150 + (y * 200))
         this.context.stroke()
     }
+
     drawO(x, y) {
         this.context.beginPath()
         this.context.strokeStyle = '#000060'
@@ -52,26 +54,96 @@ class Game {
         this.context.stroke()
         this.context.closePath()
     }
+
     addX(x, y) {
         if(this.board[x][y] === 'empty') {
             this.board[x][y] = X
             this.drawX(x, y)
-            console.log(`X: ${x} | Y: ${y}`)
-            console.log(this.board)
+            TURNS.push({ X: x, Y: y })
         }
     }
+
     addO(x, y) {
         if(this.board[x][y] === 'empty') {
             this.board[x][y] = O
             this.drawO(x, y)
-            console.log(`X: ${x} | Y: ${y}`)
-            console.log(this.board)
+            TURNS.push({ X: x, Y: y })
         }
     }
+
     reset() {
         this.board = generateGameBoard(BOARD)
-        console.log(this.board)
         this.drawDesk(this.context)
+    }
+
+    checkIFXWin(check) {
+        let result = []
+        for(var i = 0; i < 3; i++) {
+            if(check[i][0] == 'X' && check[i][1] == 'X' && check[i][2] == 'X') {
+                result.push({ x: i, y: 0 })
+                result.push({ x: i, y: 1 })
+                result.push({ x: i, y: 2 })
+            }
+            if(check[0][i] == 'X' && check[1][i] == 'X' && check[2][i] == 'X') {
+                result.push({ x: 0, y: i })
+                result.push({ x: 1, y: i })
+                result.push({ x: 2, y: i })
+            }
+        }
+        if (check[0][0] == 'X' && check[1][1] == 'X' && check[2][2] == 'X') {
+            result.push({ x: 0, y: 0 })
+            result.push({ x: 1, y: 1 })
+            result.push({ x: 2, y: 2 })
+        }
+        if (check[0][2] == 'X' && check[1][1] == 'X' && check[2][0] == 'X') {
+            result.push({ x: 0, y: 2 })
+            result.push({ x: 1, y: 1 })
+            result.push({ x: 2, y: 0 })
+        }
+        return result
+    }
+    checkIFOWin(check) {
+        let result = []
+        for(var i = 0; i < 3; i++) {
+            if(check[i][0] == 'O' && check[i][1] == 'O' && check[i][2] == 'O') {
+                result.push({ x: i, y: 0 })
+                result.push({ x: i, y: 1 })
+                result.push({ x: i, y: 2 })
+            }
+            if(check[0][i] == 'O' && check[1][i] == 'O' && check[2][i] == 'O') {
+                result.push({ x: 0, y: i })
+                result.push({ x: 1, y: i })
+                result.push({ x: 2, y: i })
+            }
+        }
+        if (check[0][0] == 'O' && check[1][1] == 'O' && check[2][2] == 'O') {
+            result.push({ x: 0, y: 0 })
+            result.push({ x: 1, y: 1 })
+            result.push({ x: 2, y: 2 })
+        }
+        if (check[0][2] == 'O' && check[1][1] == 'O' && check[2][0] == 'O') {
+            result.push({ x: 0, y: 2 })
+            result.push({ x: 1, y: 1 })
+            result.push({ x: 2, y: 0 })
+        }
+        return result
+    }
+
+    testWin() {
+        const teste = []
+        var o = []
+        teste.push(this.checkIFXWin(this.board))
+        o = this.checkIFOWin(this.board)
+        console.log(teste.length)
+        console.log(teste)
+        console.log(o.lenght)
+        if(teste.lenght > 1) {
+            console.log('X ganhou')
+        }
+        if(o.lenght > 0) {
+            console.log('O ganhou')
+        }
+        // FIXME Não funciona refazer...
     }
 }
 export default Game
--- script.js
+++ script.js
@@ -1,42 +1,34 @@
 import Game from './Game.js'
+import { TURNS } from './constants.js'
 
 const game = new Game
-let turn = 0
 document.querySelector('#newGame').addEventListener('click', () => { game.reset() })
 
 game.canvas.addEventListener('click', (event) => {
     if(event.offsetX <= 200) {
         if(event.offsetY <= 200) {
-            (turn % 2) ? game.addX(0, 0) : game.addO(0, 0)
-            turn++
+            (TURNS.length % 2) ? game.addX(0, 0) : game.addO(0, 0)
         } else if (event.offsetY <= 400) {
-            (turn % 2) ? game.addX(0, 1) : game.addO(0, 1)
-            turn++
+            (TURNS.length % 2) ? game.addX(0, 1) : game.addO(0, 1)
         } else if (event.offsetY <= 600) {
-            (turn % 2) ? game.addX(0, 2) : game.addO(0, 2)
-            turn++
+            (TURNS.length % 2) ? game.addX(0, 2) : game.addO(0, 2)
         }
     } else if (event.offsetX <= 400) {
         if(event.offsetY <= 200) {
-            (turn % 2) ? game.addX(1, 0) : game.addO(1, 0)
-            turn++
+            (TURNS.length % 2) ? game.addX(1, 0) : game.addO(1, 0)
         } else if (event.offsetY <= 400) {
-            (turn % 2) ? game.addX(1, 1) : game.addO(1, 1)
-            turn++
+            (TURNS.length % 2) ? game.addX(1, 1) : game.addO(1, 1)
         } else if (event.offsetY <= 600) {
-            (turn % 2) ? game.addX(1, 2) : game.addO(1, 2)
-            turn++
+            (TURNS.length % 2) ? game.addX(1, 2) : game.addO(1, 2)
         }
     } else if (event.offsetX <= 600) {
         if(event.offsetY <= 200) {
-            (turn % 2) ? game.addX(2, 0) : game.addO(2, 0)
-            turn++
+            (TURNS.length % 2) ? game.addX(2, 0) : game.addO(2, 0)
         } else if (event.offsetY <= 400) {
-            (turn % 2) ? game.addX(2, 1) : game.addO(2, 1)
-            turn++
+            (TURNS.length % 2) ? game.addX(2, 1) : game.addO(2, 1)
         } else if (event.offsetY <= 600) {
-            (turn % 2) ? game.addX(2, 2) : game.addO(2, 2)
-            turn++
+            (TURNS.length % 2) ? game.addX(2, 2) : game.addO(2, 2)
         }
     }
+    game.testWin()
 })
